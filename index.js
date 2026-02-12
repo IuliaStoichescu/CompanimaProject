eb64255bbfe79e643bd6209209ae3b547b5752fe
iuli <iulia.stoichescu25@gmail.com>
2026-02-12 17:28:54 +0200

    Changed the app to use express and ejs

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'assets')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+   res.render('home');
+})
+
+app.get('/about', (req, res) => {
+   res.render('about');
+})
+
+
+app.listen(8080, () => {
+   console.log("Companima server is running on port 8080");
+})
